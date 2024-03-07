26ebc8f50f1b2f8664a1b818ec229f4a7debdf70
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2024-03-07 15:10:12 -0300

    feat: add nitial header component

---
+++ app/_components/header.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+const Header = () => {
+	return (
+		<div>
+			<Card>
+				<CardContent className="flex justify-between items-center p-5">
+					<Image
+						priority
+						src={"/logo.png"}
+						alt="FSW - Barber"
+						height={20}
+						width={120}
+					/>
+					<Button variant={"outline"} size={"icon"}>
+						<MenuIcon />
+					</Button>
+				</CardContent>
+			</Card>
+		</div>
+	);
+};
+
+export default Header;
