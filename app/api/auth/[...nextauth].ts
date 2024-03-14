8e44f488b943ee1cf4f1a7af88d1de03760fef05
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2024-03-14 15:19:39 -0300

    feat: add next auth setup

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -9,8 +9,8 @@ export const authOptions = {
 	// Configure one or more authentication providers
 	providers: [
 		GoogleProvider({
-			clientId: "",
-			clientSecret: "",
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		}),
 		// ...add more providers here
 	],
